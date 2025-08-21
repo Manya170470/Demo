3d51b5058790994d62e740c2a8f9cccc49da575b
Manya170470 <mmanyaa.1704@gmail.com>
2025-08-21 20:09:21 +0530

    jagged array

---
+++ Ar.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Ar{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // Taking Jagged array as input and printing
+        System.out.print("Enter number of rows: ");
+        int rows = in.nextInt();
+
+        int[][] arr = new int[rows][];
+
+        for (int row = 0; row < rows; row++) {
+            System.out.print("Enter number of columns for row " + row + ": ");
+            int cols = in.nextInt();
+            arr[row] = new int[cols]; 
+        }
+
+        System.out.println("Enter the elements:");
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
+        }
+        System.out.println("The jagged array is:");
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
